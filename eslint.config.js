dc5338ecab598758a782adc7fb81d636506b8716
Jeeyoung Kim <jeeyoungk@gmail.com>
2025-08-10 08:46:20 -0700

    format and lint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import globals from 'globals';
 
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ['dist/**', 'docs/**', 'node_modules/**'],
   },
   js.configs.recommended,
   {
